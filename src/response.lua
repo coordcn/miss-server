ad1310a6f9494ea3a5d2bdf1bacb33c3a5b698d7
coordcn <coordcn@163.com>
2017-06-22 18:14:53 +0800

    update

--- src/response.lua
+++ src/response.lua
@@ -2,54 +2,110 @@
 -- @author      QianYe(coordcn@163.com)
 -- @license     MIT license
 
-local core = require("miss-core")
-local Object = core.Object
+local handles   = require("miss-server.src.handles")
+local core      = require("miss-core")
+local Object    = core.Object
+local MIME      = core.MIME
 
 local Response = Object:extend()
 
+-- @property    type    {string}                        mime type
+-- @property    charset {string|default: utf-8}         charset
+-- @property    status  {number}                        http status
+-- @property    error   {string}                        error
 function Response:constructor()
+        self._headers = {}
+        self._cookies = {}
 end
 
-function Response:setHeader()
+function Response:setHeader(key, value)
+        self._headers[key] = value
 end
 
-function Response:setCookie()
+function Response:setCookie(key, value)
+        self._cookies[key] = value
 end
 
--- @param       status  {number} 
-function Response:status(status)
-        self.status = status
+-- @param       input   {string}
+function Response:body(input, mime)
+        self._handle = "body"
+        self._input = input
+        if mime then
+                self.type = mime
+        end
 end
 
--- @param       mime    {string}
-function Response:type(mime)
-        self.type = mime
+-- @param       input   {object}
+--              {
+--                      url     = {string}
+--                      status  = {number}
+--                      body    = {string}
+--                      type    = {string}
+--              }
+function Response:redirect(input)
+        self._handle = "redirect"
+        self._input = input
+        if mime then
+                self.type = mime
+        end
 end
 
--- @param       body    {string}
-function Response:body(body)
-        self.type = body
+-- @brief       for test, not for product, maybe block the socket
+-- @param       input   {object}
+--              {
+--                      inline          = {boolean|default: false}
+--                      filename        = {string}
+--                      extname         = {string}
+--                      path            = {string}
+--                      data            = {string}
+--                      type            = {string}
+--              }
+function Response:download(input)
+        self._handle = "download"
+        self._input = input
+        if mime then
+                self.type = mime
+        end
 end
 
-function Response:redirect(url, status)
+-- @param       input   {string}
+function Response:html(input)
+        self._handle = "html"
+        self._input = input
 end
 
-function Response:download()
+-- @param       input   {string}
+function Response:text(input)
+        self._handle = "text"
+        self._input = input
 end
 
-function Response:html()
+-- @param       input   {object|array}
+function Response:json(input)
+        self._handle = "json"
+        self._input = input
 end
 
-function Response:text()
+-- @param       input   {object}
+function Response:xml(input)
+        self._handle = "xml"
+        self._input = input
 end
 
-function Response:json()
+-- @param       callback        {string}
+-- @param       input           {object|array}
+function Response:jsonp(callback, input)
+        self._handle = "jsonp"
+        self._callback = callback
+        self._input = input
 end
 
-function Response:xml()
-end
+function Response:_output()
+        local handle = handles[self._handle]
 
-function Response:jsonp()
+        if handle then
+               return handle(self) 
+        end
 end
 
 return Response
